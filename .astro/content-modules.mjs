9d0ce660c0f34648f5338e474b8b6a431819ea91
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-12-11 21:32:47 +0000

    bump deps to latest + build

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/blog/kitchensink.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fkitchensink.mdx&astroContentModuleFlag=true")]]);
+		
