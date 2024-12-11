5c928a50c8acf1780155715fc6efcdfaac582c7c
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2024-12-11 19:19:45 +0000

    rough content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,9 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#FFD000",
+      },
       fontFamily: {
         sans: [
           "Bricolage Grotesque Variable",
